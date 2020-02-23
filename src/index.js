991315cb091a3aea55c520e354510545a95ff04c
Olawale Sunmonu-Adedeji <olawalesunmonu-adedeji@Olawales-MacBook-Pro.local>
2020-02-23 14:51:49 +0100

    Action creators and higher order components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter,Route} from 'react-router-dom';
+import Welcome from './components/Welcome';
+import SignUp from './components/auth/SignUp';
+import Feature from './components/Feature';
+import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import {createStore,applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import reducers from './reducers';
+import App from './components/App';
+import reduxThunk from 'redux-thunk';
+
+
+const store = createStore(reducers,{
+    auth: {
+        authenticated: localStorage.getItem('token')
+    }
+},applyMiddleware(reduxThunk)) 
+
+ReactDOM.render(
+    <Provider store={store}>
+    <BrowserRouter>
+     <App >
+        <Route path = "/" exact component={Welcome}/>
+        <Route path = "/signup" exact component={SignUp}/>
+        <Route path = "/feature" exact component={Feature}/>
+        <Route path = "/signout" exact component={Signout}/>
+        <Route path= "/signin" exact component={Signin}/>
+    </App>
+    </BrowserRouter>
+    </Provider>,
+    document.querySelector('#root')
+)
